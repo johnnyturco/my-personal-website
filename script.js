3cc795e9896ef7c3d9c6edd457f9f584defdecb3
Johnny <johnnyturco@me.com>
2022-10-20 12:04:18 -0500

    add basic html, css, and js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+// document.querySelector("body").appendChild(h2);
+
+const articleTitle = document.querySelector('.articleTitle');
+const linkField = document.querySelector('.linkField');
+const linkButton = document.querySelector('.linkButton');
+const contentContainer = document.querySelector('.contentContainer');
+
+linkButton.addEventListener('click', function() {
+  addLink(articleTitle.value,linkField.value);
+});
+
+function addLink(title,link) {
+  const newLink = document.createElement('h2');
+  newLink.innerHTML = `<a href="${link}">${title}</a>`
+  newLink.className = 'article';
+  contentContainer.appendChild(newLink);
+}
